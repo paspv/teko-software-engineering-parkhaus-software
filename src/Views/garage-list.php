661253ecfe79815ab6cbe946024357ef9e3e5589
paspv <pascalblaser@bluewin.ch>
2026-05-01 20:24:55 +0200

    Added styling to homepage

--- src/Views/garage-list.php
+++ src/Views/garage-list.php
@@ -5,20 +5,22 @@
 /** @var array $garages */
 ?>
 
-<h3>Alle Parkhäuser:</h3>
-<ul>
-    <?php if (empty($garages)): ?>
-        <li>Es wurden keine Parkhäuser gefunden.</li>
-    <?php else: ?>
+<div class="parking-garage-selection">
+    <h3>Alle Parkhäuser:</h3>
+    <ul class="parking-garage-seleciton-list">
+        <?php if (empty($garages)): ?>
+            <li>Es wurden keine Parkhäuser gefunden.</li>
+        <?php else: ?>
 
-        <?php foreach ($garages as $garage): ?>
-            <li>
-                <strong><?= $garage->Name ?></strong> 
-                — <a href="/<?= $garage->Id ?>/entry">Go to Entry</a>
-            </li>
-        <?php endforeach; ?>
-    <?php endif; ?>
-</ul>
+            <?php foreach ($garages as $garage): ?>
+                <li>
+                    <a href="/<?= $garage->Id ?>/entry"><?= $garage->Name ?></a>
+                </li>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </ul>
+</div>
+
 
 <?php include __DIR__ . '/layout/footer.php'; ?>
 
